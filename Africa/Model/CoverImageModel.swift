bb541c3bc361149cdacc9428582e65f7f005b073
Itunu Raimi <chavitunu@gmail.com>
2021-02-16 09:23:28 +0100

    designed cover image view and decoded json file

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Itunu Raimi on 16/02/2021.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
