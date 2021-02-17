3559e31d2f166faf67907090a7b15bc153f2ef55
Itunu Raimi <chavitunu@gmail.com>
2021-02-17 09:50:55 +0100

    designed grid for various animal images

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,15 +8,45 @@
 import SwiftUI
 
 struct GalleryView: View {
+    // MARK: - PROPERTIES
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    // Simple Grid Definition
+//    let gridLayout: [GridItem] = [
+//        GridItem(.flexible()),
+//        GridItem(.flexible()),
+//        GridItem(.flexible())
+//    ]
+    
+    // Efficient Grid Definition
+    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    
+    // MARK: - BODY
     var body: some View {
         ScrollView(.vertical, showsIndicators: false, content: {
-            /*@START_MENU_TOKEN@*/Text("Placeholder")/*@END_MENU_TOKEN@*/
+            // MARK: - GRID
+            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10, content: {
+                ForEach(animals) { animal in
+                    Image(animal.image)
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(Circle())
+                        .overlay(
+                            Circle()
+                                .stroke(Color.white, lineWidth: 1)
+                        )
+                }
+                
+            }) //: GRID
+                
         }) //: SCROLL
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
     }
 }
 
+// MARK: - PREVIEW
 struct GalleryView_Previews: PreviewProvider {
     static var previews: some View {
         GalleryView()
