3d5a2e62664aedef4c429d1621c6d57c222350aa
Itunu Raimi <chavitunu@gmail.com>
2021-02-17 10:19:42 +0100

    created basic grid layout

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -10,35 +10,71 @@ import SwiftUI
 struct GalleryView: View {
     // MARK: - PROPERTIES
     let animals: [Animal] = Bundle.main.decode("animals.json")
+    @State private var selectedPhoto = "lion"
     
-    // Simple Grid Definition
+// Simple Grid Definition
 //    let gridLayout: [GridItem] = [
 //        GridItem(.flexible()),
 //        GridItem(.flexible()),
 //        GridItem(.flexible())
 //    ]
     
-    // Efficient Grid Definition
-    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+// Efficient Grid Definition
+//    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+// Dynamic Grid Definition
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn = 3.0
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
     
     
     // MARK: - BODY
     var body: some View {
         ScrollView(.vertical, showsIndicators: false, content: {
-            // MARK: - GRID
-            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10, content: {
-                ForEach(animals) { animal in
-                    Image(animal.image)
-                        .resizable()
-                        .scaledToFit()
-                        .clipShape(Circle())
-                        .overlay(
-                            Circle()
-                                .stroke(Color.white, lineWidth: 1)
-                        )
-                }
+            
+            VStack(alignment: .center, spacing: 30) {
+                // MARK: - IMAGE
                 
-            }) //: GRID
+                Image(selectedPhoto)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle()
+                            .stroke(Color.white, lineWidth: 8)
+                    )
+                
+                // MARK: - SLIDER
+                Slider(value: $gridColumn, in: 2...4)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn, perform: { value in
+                        gridSwitch()
+                    })
+                
+                // MARK: - GRID
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10, content: {
+                    ForEach(animals) { animal in
+                        Image(animal.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(
+                                Circle()
+                                    .stroke(Color.white, lineWidth: 1)
+                            )
+                            .onTapGesture {
+                                selectedPhoto = animal.image
+                            }
+                    }
+                }) //: GRID
+                .onAppear(perform: {
+                    gridSwitch()
+                })
+            } //: VSTACK
                 
         }) //: SCROLL
         .frame(maxWidth: .infinity, maxHeight: .infinity)
